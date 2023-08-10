c0ca87a94b48bfdddf95e19b8c2747753d667ff2
John E <techryon@gmail.com>
2023-08-10 22:48:38 +1200

    Complete - data from API

--- src/App.js
+++ src/App.js
@@ -1,55 +1,32 @@
-import React from "react";
+//https://www.youtube.com/watch?v=bZXjHauDNcg&ab_channel=ChrisBlakely
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import ContactCard from "./ContactCard";
 
 const App = () => {
-  const contacts = [
-    {
-      avatarUrl: "https://via.placeholder.com/150",
-      name: "Jenny Han",
-      email: "jenny.han@notreal.com",
-      age: "25",
-    },
-    {
-      avatarUrl: "https://via.placeholder.com/150",
-      name: "Paul Jones",
-      email: "paul.jones@notreal.com",
-      age: "32",
-    },
-    {
-      avatarUrl: "https://via.placeholder.com/150",
-      name: "Peter Pan",
-      email: "peter.pan@neverland.com",
-      age: "100",
-    },
-  ];
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+  }, []);
 
   return (
     <div>
-      {contacts.map((contact, index) => {
+      {results.map((result, index) => {
         return (
           <ContactCard
             key={index}
-            avatarUrl={contact.avatarUrl}
-            name={contact.name}
-            email={contact.email}
-            age={contact.age}
+            avatarUrl={result.picture.large}
+            name={result.name.first}
+            email={result.email}
+            age={result.dob.age}
           />
         );
       })}
-
-      {/* <ContactCard
-        avatarUrl='https://via.placeholder.com/150'
-        name='Paul Jones'
-        email='paul.jones@notreal.com'
-        age='32'
-      />
-      <ContactCard
-        avatarUrl='https://via.placeholder.com/150'
-        name='Peter Pan'
-        email='peter.pan@neverland.com'
-        age='100'
-      /> */}
     </div>
   );
 };
